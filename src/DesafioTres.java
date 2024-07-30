32da2ba7aceac064b5d81a6b0279415f6612e3f4
Alejandro Parias <alejandroparmo@gmail.com>
2024-07-30 18:55:26 -0500

    MEJORAS EN EL DESAFIO 3

--- src/DesafioTres.java
+++ src/DesafioTres.java
@@ -3,27 +3,32 @@ import java.util.Scanner;
 
 public class DesafioTres {
     public static void main(String[] args) {
-        //  PARA SOLICITAR LA ENTRADA AL USUARIO DEL TEXTO Y DEMÁS DATOS
+        //  PARA SOLICITAR LA ENTRADA POR CONSOLA DE LA CANTIDAD DE NUM DEM ARRAY Y SUS VALORES
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Introduce la cantidad de monedas para crear el array o presiona Enter para usar datos por defecto");
         String input = scanner.nextLine();
 
-        //  VARIABLE QUE ES DE TIPO ARRAY
+        //  VARIABLE DE TIPO ARRAY QUE CONTENDRÁ LOS VALORES DE LAS MONEDAS
         int[] coins;
 
-        // VALIDA SI EL DATO INSERTADO ES VACÍO
+        // VALIDA SI EL DATO INSERTADO POR CONSOLA ES VACÍO
         if (input.isEmpty()) {
             // DATOS POR DEFECTO POR SI LA ARRAY NO ES ALIMENTADA
-            coins = new int[] {1, 2, 4, 9};
+            coins = new int[]{1, 2, 4, 9};
             System.out.println("No se ingresaron monedas. Usando datos por defecto:");
         } else {
+            //  VARIABLE QUE TOMARÁ EL VALOR INGRESADO POR CONSOLA PARA SABER EL TAMAÑO DEL ARRAY
             int n = 0;
+            //  VARIABLE CENTINELA QUE SIRVE PARA SALIR DEL WHILE
             boolean validInput = false;
 
+            //  MIENTRAS LA VARIABLE CENTINELA SEA NEGATIVA SE INGRESARÁ AL WHILE
             while (!validInput) {
                 try {
+                    //  VALIDA SI EL DATO INGRESADO ES NUMÉRICO PARA ASIGNARLO A LA VARIABLE
                     n = Integer.parseInt(input);
+                    //
                     validInput = true;
                 } catch (NumberFormatException e) {
                     System.out.println("Entrada no válida, introduce un número:");
@@ -31,21 +36,26 @@ public class DesafioTres {
                 }
             }
 
+            //  SE ASIGNA EL TAMAÑO DEL ARRAY A LA LIST coins
             coins = new int[n];
 
             //  ITERAR Y ALIMENTAR EL ARRAY
             for (int i = 0; i < n; i++) {
+                //  VARIABLE CENTINELA QUE SIRVE PARA SALIR DEL WHILE
                 boolean validCoinInput = false;
 
+                //  MIENTRAS LA VARIABLE CENTINELA SEA NEGATIVA SE INGRESARÁ AL WHILE
                 while (!validCoinInput) {
                     System.out.println("Introduce el valor de la moneda que contendrá el array y da enter");
                     try {
+                        //  VALIDA SI EL DATO INGRESADO ES NUMÉRICO PARA ASIGNARLO A LA VARIABLE
                         int numero = Integer.parseInt(scanner.nextLine());
 
-                        //  VALIDAR QUE LOS NÚMEROS INGRESADOS SEAN POSITIVOS
+                        //  VALIDAR QUE LOS VALORES INGRESADOS POR CONSOLA SEAN POSITIVOS
                         if (numero < 0) {
                             System.out.println("Los valores de las monedas deben ser positivos");
                         } else {
+                            //  AL PUESTO DEL ARRAY SE LE DA EL VALOR INGRESADO
                             coins[i] = numero;
                             validCoinInput = true;
                         }
@@ -60,28 +70,28 @@ public class DesafioTres {
         // ORDENAR EL ARRAY CON EL MÉTODO PROPIO DE JAVA
         Arrays.sort(coins);
 
-        // MOSTRAR EL ARRAY DE MONEDAS
+        // MOSTRAR EL ARRAY DE VALORES DE MONEDAS ORGANIZADO
         System.out.println("Monedas utilizadas:");
         for (int coin : coins) {
             System.out.print(coin + " ");
         }
-        System.out.println(); // Para un salto de línea
+        System.out.println(); // SALTO DE LÍNEA
 
         //  LLAMADO AL MÉTODO QUE OBTENDRÁ EL VALOR MÍNIMO DE CAMBIO QUE NO SE PUEDE DAR
         int minChange = findMinChange(coins);
         System.out.println("La cantidad mínima de cambio que no se puede dar es: " + minChange);
 
+        //  CIERRE DE SCANNER
         scanner.close();
 
-        // EJECUTAR LAS PRUEBAS
+        // EJECUTAR LAS PRUEBAS UNITARIAS
         ejecutarPruebas();
     }
 
 
-
     //  MÉTODO QUE OBTENDRÁ EL VALOR MÍNIMO DE CAMBIO QUE NO SE PUEDE DAR DE LAS MONEDAS QUE HAY
     public static int findMinChange(int[] coins) {
-        // ORDENAR LAS MONEDAS
+        // ORDENAR LAS MONEDAS POR SU VALOR
         Arrays.sort(coins);
 
         //  VARIABLE DE SEGUIMIENTO DEL CAMBIO MÁXIMO CON LAS MONEDAS QUE HAY
@@ -89,9 +99,9 @@ public class DesafioTres {
 
         //  ITERAR CADA MONEDA
         for (int coin : coins) {
-            // SI LA MONEDA ES MAYOR AL CAMBIO, NO SE PODRÍA DAR EL CAMBIO
+            // SI LA MONEDA ES MAYOR A LA VARIABLE DE SEGUIMIENTO + 1, NO SE PODRÍA DAR EL CAMBIO
             if (coin > currentChange + 1) {
-                break;
+                return  currentChange + 1;
             }
             //  SUMAR EL VALOR DE LA MONEDA AL CAMBIO EXISTENTE
             currentChange += coin;
@@ -101,7 +111,7 @@ public class DesafioTres {
         return currentChange + 1;
     }
 
-    //  MÉTODO DE PRUEBA
+    //  MÉTODO QUE EJECUTA LAS PRUEBAS UNITARIAS
     public static void ejecutarPruebas() {
         System.out.println("-----------------------------------------------------------------------------------------");
         System.out.println("-----------------------------------------------------------------------------------------");
